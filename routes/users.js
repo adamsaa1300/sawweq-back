4fe156a396e849b22529c838deb3e07abe85505e
SalyHasayen <s12400126@stu.najah.edu>
2026-05-14 13:58:18 +0300

    up

--- routes/users.js
+++ routes/users.js
@@ -407,4 +407,67 @@ router.put("/reset-password", async (req, res) => {
     }
 
 });
+
+router.post('/google-login', async (req, res) => {
+
+    try {
+
+        const { name, email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+
+            user = new User({
+                name,
+                email,
+                password: "google-auth-user",
+                birthDate: new Date(),
+                location: "Unknown",
+                faculty: "Unknown",
+                uni: "Unknown",
+                role: "user"
+            });
+
+            await user.save();
+
+        }
+
+        const token = jwt.sign(
+
+            {
+                id: user._id,
+                role: user.role
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+                expiresIn: "7d"
+            }
+
+        );
+
+        res.json({
+
+            token,
+
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            error: err.message
+        });
+
+    }
+
+});
 module.exports = router
